bf215dbda0c3cbc4396ef3d89d6a6a7700a84dd4
Michael Nwogu <mikedbchi@gmail.com>
2021-09-08 05:57:43 +0100

    update

---
+++ src/matter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import reactDOM, { render } from "react-dom";
+
+class Car extends React-Component{
+
+  render(){
+    return <h2>I am a car</h2>;
+  }
+
+}
+
+export default Car;
